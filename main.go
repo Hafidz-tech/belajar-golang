30d3442724cf68e2688202e81d7566f7689a8d17
Hafidz-tech <hafidzmagfiroh633@gmail.com>
2025-09-25 14:35:56 +0700

    update program sederhana

--- main.go
+++ main.go
@@ -1,99 +1,98 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"net/http"
-	"strconv"
-
-	"github.com/gorilla/mux"
 )
 
-// Struct User
-type User struct {
-	ID   int    `json:"id"`
-	Name string `json:"name"`
-}
-
-// Data sementara (slice)
-var users []User
-var nextID = 1
-
-// GET: ambil semua user
-func getUsers(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(users)
-}
-
-// GET: ambil user by ID
-func getUser(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	id, _ := strconv.Atoi(params["id"])
-
-	for _, user := range users {
-		if user.ID == id {
-			w.Header().Set("Content-Type", "application/json")
-			json.NewEncoder(w).Encode(user)
-			return
-		}
-	}
-	http.NotFound(w, r)
-}
-
-// POST: tambah user
-func createUser(w http.ResponseWriter, r *http.Request) {
-	var newUser User
-	json.NewDecoder(r.Body).Decode(&newUser)
-
-	newUser.ID = nextID
-	nextID++
-	users = append(users, newUser)
-
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(newUser)
-}
-
-// PUT: update user
-func updateUser(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	id, _ := strconv.Atoi(params["id"])
-
-	for i, user := range users {
-		if user.ID == id {
-			json.NewDecoder(r.Body).Decode(&users[i])
-			users[i].ID = id // jangan ganti ID
-			w.Header().Set("Content-Type", "application/json")
-			json.NewEncoder(w).Encode(users[i])
-			return
-		}
-	}
-	http.NotFound(w, r)
-}
-
-// DELETE: hapus user
-func deleteUser(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	id, _ := strconv.Atoi(params["id"])
-
-	for i, user := range users {
-		if user.ID == id {
-			users = append(users[:i], users[i+1:]...)
-			w.WriteHeader(http.StatusNoContent)
-			return
-		}
-	}
-	http.NotFound(w, r)
-}
+var data []string // slice untuk simpan data
 
 func main() {
-	r := mux.NewRouter()
+	for {
+		fmt.Println("\n===== MENU =====")
+		fmt.Println("1. Tambah Data")
+		fmt.Println("2. Lihat Data")
+		fmt.Println("3. Update Data")
+		fmt.Println("4. Hapus Data")
+		fmt.Println("5. Keluar")
+		fmt.Print("Pilih menu: ")
 
-	r.HandleFunc("/users", getUsers).Methods("GET")
-	r.HandleFunc("/users/{id}", getUser).Methods("GET")
-	r.HandleFunc("/users", createUser).Methods("POST")
-	r.HandleFunc("/users/{id}", updateUser).Methods("PUT")
-	r.HandleFunc("/users/{id}", deleteUser).Methods("DELETE")
+		var pilih int
+		fmt.Scanln(&pilih)
 
-	fmt.Println("Server running on http://localhost:8080")
-	http.ListenAndServe(":8080", r)
+		switch pilih {
+		case 1:
+			tambahData()
+		case 2:
+			lihatData()
+		case 3:
+			updateData()
+		case 4:
+			hapusData()
+		case 5:
+			fmt.Println("Keluar dari program...")
+			return
+		default:
+			fmt.Println("Pilihan tidak valid")
+		}
+	}
+}
+
+func tambahData() {
+	var nama string
+	fmt.Print("Masukkan nama: ")
+	fmt.Scanln(&nama)
+	data = append(data, nama)
+	fmt.Println("Data berhasil ditambahkan!")
+}
+
+func lihatData() {
+	fmt.Println("\n===== DATA =====")
+	if len(data) == 0 {
+		fmt.Println("Belum ada data.")
+		return
+	}
+	for i, v := range data {
+		fmt.Printf("%d. %s\n", i+1, v)
+	}
+}
+
+func updateData() {
+	lihatData()
+	if len(data) == 0 {
+		return
+	}
+
+	var index int
+	fmt.Print("Masukkan nomor data yang mau diupdate: ")
+	fmt.Scanln(&index)
+
+	if index < 1 || index > len(data) {
+		fmt.Println("Nomor tidak valid!")
+		return
+	}
+
+	var namaBaru string
+	fmt.Print("Masukkan nama baru: ")
+	fmt.Scanln(&namaBaru)
+	data[index-1] = namaBaru
+	fmt.Println("Data berhasil diupdate!")
+}
+
+func hapusData() {
+	lihatData()
+	if len(data) == 0 {
+		return
+	}
+
+	var index int
+	fmt.Print("Masukkan nomor data yang mau dihapus: ")
+	fmt.Scanln(&index)
+
+	if index < 1 || index > len(data) {
+		fmt.Println("Nomor tidak valid!")
+		return
+	}
+
+	data = append(data[:index-1], data[index:]...)
+	fmt.Println("Data berhasil dihapus!")
 }
